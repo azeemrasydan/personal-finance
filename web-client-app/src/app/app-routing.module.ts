2fa04784cb05f7c2070c838a162b1b45a4cb390c
Azeem Rasydan <azeem.rasydan@plus.com.my>
2023-05-13 22:04:56 +0800

    Added plan, simulation and journey pages with 'ng generate component pages/<page-name>'

--- web-client-app/src/app/app-routing.module.ts
+++ web-client-app/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JourneyComponent } from './pages/journey/journey.component';
+import { SimulationComponent } from './pages/simulation/simulation.component';
+import { PlanComponent } from './pages/plan/plan.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'journey',
+    title: 'Financial Journey',
+    component: JourneyComponent
+  },
+  {
+    path: 'simulation',
+    title: 'Financial Simulation',
+    component: SimulationComponent
+  },
+  {
+    path: 'plan',
+    title: 'Financial Plan',
+    component: PlanComponent
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web-client-app/src/app/app.module.ts
+++ web-client-app/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeNavbarComponent } from './shared/home-navbar/home-navbar.component';
+import { JourneyComponent } from './pages/journey/journey.component';
+import { SimulationComponent } from './pages/simulation/simulation.component';
+import { PlanComponent } from './pages/plan/plan.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeNavbarComponent
+    HomeNavbarComponent,
+    JourneyComponent,
+    SimulationComponent,
+    PlanComponent
   ],
   imports: [
     BrowserModule,
