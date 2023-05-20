31109a8ae7b867f6c35a65f32d74206d7f1c35bd
Azeem Rasydan <azeem.rasydan@plus.com.my>
2023-05-21 00:17:10 +0800

    On-going: Developing a journey module

--- web-client-app/src/app/pages/journey/journey.component.ts
+++ web-client-app/src/app/pages/journey/journey.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class JourneyComponent {
 
+  public journeyCreatingShown: boolean = false;
+
+  createAJourneyButtonOnClick() {
+    this.journeyCreatingShown = true;
+  }
+
+  public annualIncome: number = 0;
+  public monthlyDebt: number = 0;
+  public interestRate: number = 0;
+  public loanTerm: number = 0;
+  public showAffordability: boolean = false;
+  public affordabilityResult: boolean = false;
+
+  calculateAffordability() {
+
+    const monthlyIncome = this.annualIncome / 12;
+    const totalDebt = this.monthlyDebt * 12;
+    const monthlyInterestRate = this.interestRate / 100 / 12;
+    const loanTermInMonths = this.loanTerm * 12;
+
+    const loanAmount = (monthlyIncome - totalDebt) * (1 - Math.pow(1 + monthlyInterestRate, -loanTermInMonths)) / monthlyInterestRate;
+
+    this.affordabilityResult = loanAmount > 0;
+    this.showAffordability = true;
+  }
+
+  parse(value: any): number {
+    return parseFloat(value);
+  }
 }
