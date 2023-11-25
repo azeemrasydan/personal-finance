a39b9bbe4cb1aee1f5d8c4befa9a9e365491eb60
Azeem Rasydan <azeem.rasydan@plus.com.my>
2023-11-25 23:47:51 +0800

    Tried simulate on main,

--- play/src/main/java/azeem/play/App.java
+++ play/src/main/java/azeem/play/App.java
@@ -1,6 +1,12 @@
 package azeem.play;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Scanner;
+
+import azeem.play.models.Income;
+import azeem.play.models.Person;
 
 /**
  * Hello world!
@@ -14,7 +20,6 @@ public class App {
         System.out.println("================================================");
 
         mainProgram();
-
 
         System.out.println("<================================================>");
         System.out.println("<================================================>");
@@ -31,7 +36,28 @@ public class App {
         }
     }
 
+    private static Date dateOf(String dateString) {
+        return java.sql.Date.valueOf(
+                LocalDate.parse(dateString, DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+    }
+
     public static void mainProgram() {
+        Person john = new Person(
+                "azeem",
+                "rasydan",
+                dateOf("1995-07-22"),
+                "asd");
+
+        john.addRecurringIncome(
+                "monthly",
+                26,
+                dateOf("2021-01-01"),
+                dateOf("2025-07-07"),
+                3000);
+
+        for (Income income : john.incomes()) {
+            System.out.println(income.getAmount());
+        }
 
     }
 }
